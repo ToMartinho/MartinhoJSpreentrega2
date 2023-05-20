e59731f2a4859a6ac1b00cd4892ad29bd7675c1f
ToMartinho <tome.martinho@gmail.com>
2023-05-20 15:54:33 -0300

    se agrega al codigo verificaicion de reservas mejorada

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* ----------------------------------------------------  CLASES  --------------------------------------------------*/
+
 // se crea la clase de RESERVA
 class Reserva{
     constructor(nombre,apelliodo,dni,dia,horario){
@@ -8,36 +10,23 @@ class Reserva{
         this.horario = horario;
     }
 }
+/* ----------------------------------------------------  FIN CLASES  -----------------------------------------------*/ 
 
-// el usuario realiza la reserva
-const reservas = [];
-let continuar = true;
-while(continuar){
-    const nombre = prompt("Ingrese su nombre").toUpperCase();
-    const apellido = prompt("Ingrese su Apellido").toUpperCase();    
-    const dni = parseInt(prompt("Ingrese su DNI"));
-    const dia = prompt("seleccione un dia entre lunes a domingo para su reserva");
-    const horario = parseInt(prompt("un horario entre las 10hs hasta las 23hs para su reserva"));
-    // verificarReserva(dia,horario);   
-    reservas.push(new Reserva(nombre,apellido,dni,dia,horario));
-    continuar = confirm("Quiere realizar otra reserva?");
 
-}
-console.log(reservas);
+
+/* ----------------------------------------------------  FUNCIONES  ------------------------------------------------*/  
 // se crea funcion para verificar que no exista ya una resesrva para ese dia y horario
-const verificarReserva=(dia,horario)=>{
-    // se buscan del dia y el horario
-    const indexDia = reservas.findIndex((reserva)=> reserva.dia == dia);
-    const indexHorario = reservas.findIndex((reserva)=> reserva.horario == horario);
-    if(indexDia && indexHorario != -1){
-        // si el horario ya se encuentra reservado
-        alert("la fecha o el horario ya se encuentran reservados por alguien mas");
-        const dia = prompt("a continuacion seleccione nuevamente el dia");
-        const horario = parseInt(prompt("seleccione un nuevo horario para su reserva"));
-        return dia,horario; 
-    }else{
-        alert("su reserva se a realizado exitosamente");
-    }
+const verificarReserva=(reservas,dia,horario)=>{
+    // se buscan el dia y el horario
+    reservas.forEach((reserva)=>{
+        if(reserva.dia == dia && reserva.horario == horario){
+            // si el horario ya se encuentra reservado
+            return true;
+        }else{
+            // si el horario esta disponible
+            return false;
+        }
+    })
 }
 
 // se crea funcion para cancelar la reserva
@@ -50,7 +39,48 @@ const cancelarReserva=(dni)=>{
         alert("el dni ingresado no posee ninguna reserva");
     }
 }
+/* ----------------------------------------------------  FIN FUNCIONES  -----------------------------------------------*/  
+
+/* -----------------------------------------------------  MAIN  -------------------------------------------------------*/ 
+
+/* -------------  VARIABLES  -----------*/  
+
+const reservas = [];
+let continuar = true;
+
+/* ------------  FIN VARIABLES  ---------*/  
+
+// el usuario realiza la reserva
+while(continuar){
+    const nombre = prompt("Ingrese su nombre").toUpperCase();
+    const apellido = prompt("Ingrese su Apellido").toUpperCase();    
+    const dni = parseInt(prompt("Ingrese su DNI"));
+    const dia = prompt("seleccione un dia entre lunes a domingo para su reserva");
+    const horario = parseInt(prompt("un horario entre las 10hs hasta las 23hs para su reserva"));
+    if(reservas == 0){
+       reservas.push(new Reserva(nombre,apellido,dni,dia,horario));
+       alert("dia y horario reservado correctamente");
+       console.log(reservas);
+    }else{
+       if(verificarReserva(reservas,dia,horario) == false){
+            reservas.push(new Reserva(nombre,apellido,dni,dia,horario));
+            alert("dia y horario se a reservado correctamente");
+            console.log(reservas);
+       }else{
+            alert("el dia y/o el horario se encuentran ocupados a continuacion elija un nuevo dia o horario");
+            const dia = prompt("seleccione un dia entre lunes a domingo para su reserva");
+            const horario = parseInt(prompt("un horario entre las 10hs hasta las 23hs para su reserva"));
+            console.log(dia);
+            console.log(horario);
+            console.log(reservas);
+       }
+    }
+
+    continuar = confirm("Quiere realizar otra reserva?");
+
+}
 console.log(reservas);
+
 // se le pregunta al usuario si quere eliminar una reserva
 continuar = confirm("Quiere cancelar alguna reserva ? ");
 while(continuar){
@@ -59,3 +89,6 @@ while(continuar){
     continuar = confirm("Quiere cancelar alguna otra reserva?");
 }
 console.log(reservas);
+
+
+/* ----------------------------------------------------  FIN MAIN  ----------------------------------------------------*/  
